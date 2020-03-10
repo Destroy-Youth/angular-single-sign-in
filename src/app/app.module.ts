1bc3e22ba5792a7b8d2a0f3a0193fe6dd542b4fc
Herrera <Luisivan.Herrera.consultant@nielsen.com>
2020-03-10 13:34:11 -0600

    User info display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LogginComponent } from './loggin/loggin.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogginComponent
+    LogginComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule
--- src/app/loggin/loggin.component.ts
+++ src/app/loggin/loggin.component.ts
@@ -15,6 +15,11 @@ export class LogginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.googleInit();
+
+  }
+
   public auth2: any;
   public googleInit() {
     gapi.load('auth2', () => {
@@ -30,16 +35,9 @@ export class LogginComponent implements OnInit {
   public attachSignin(element) {
     this.auth2.attachClickHandler(element, {},
       (googleUser) => {
-
-        let profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-        console.log(googleUser);
-
-        //YOUR CODE HERE
+        console.log('User logged');
+        let id_token = googleUser.getAuthResponse();
+        console.log(id_token);
 
 
       }, (error) => {
@@ -52,11 +50,6 @@ export class LogginComponent implements OnInit {
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-  }
-
-  ngAfterViewInit() {
-    this.googleInit();
-
   }
 
 }
